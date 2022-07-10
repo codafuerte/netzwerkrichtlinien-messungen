7fb5443952b1fbd4cfef68a4574e8dd9021d3b82
Andre Farwick <andre.farwick@fh-muenster.de>
2022-07-10 16:02:55 +0200

    extract raw data of mem and cpu usage

--- scripts/extract-cpu-and-mem-usage.sh
+++ scripts/extract-cpu-and-mem-usage.sh
@@ -5,8 +5,10 @@ echo $FILES
 for file in "${FILES[@]}"
 do
     echo $file
-    NEW_FILE_MEM=$( echo "$file" | sed 's/top.txt/top_mem.txt/' )
-    NEW_FILE_CPU=$( echo "$file" | sed 's/top.txt/top_cpu.txt/' )
+    NEW_FILE_MEM=$( echo "$file" | sed 's/top.txt/top-mem.txt/' )
+    NEW_FILE_CPU=$( echo "$file" | sed 's/top.txt/top-cpu.txt/' )
     cat ${file} | grep -P "(MiB Mem).*" > ${NEW_FILE_MEM}
     cat ${file} | grep -P "(%Cpu).*" > ${NEW_FILE_CPU}
+    sudo sed -i 's/[^0-9\.,]/''/g' $NEW_FILE_CPU
+    sudo sed -i 's/[^0-9\.,]/''/g' $NEW_FILE_MEM
 done
